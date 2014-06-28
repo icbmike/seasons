f593bf78b4532d9807ea57f5681093c5a12632dc
Michael Little <bio595@gmail.com>
2014-06-28 21:01:36 +1200

    have a character that can jump

---
+++ Seasons/Assets/InputBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var jumpButton = Input.GetButton ("Jump");
+
+		if(jumpButton)
+			rigidbody2D.AddForce(Vector2.up * 10);
+	}
+}
